86ef3724012e91b213a6a4338f4773e99b255c02
Shreyash <siddheshj039@gmail.com>
2024-09-06 10:48:27 +0530

    made some changes in addcourse

--- app/dashboard/_components/AddCourse.jsx
+++ app/dashboard/_components/AddCourse.jsx
@@ -17,7 +17,7 @@ function AddCourse() {
             <span className='font-bold'>{user?.fullName}</span></h2>
             <p className='text-sm text-gray-500'>Create new course with AI, Share with friends and Earn from it</p>
         </div>
-        <Link href={userCourseList?.length>=5?'https://www.instgramita.com':'/create-course'}>
+        <Link href={userCourseList?.length>=5?'https://ai-cousre-generator.vercel.app':'/create-course'}>
              <Button>+ Create AI Course</Button>
         </Link>
     </div>
